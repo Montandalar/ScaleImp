13153ad168d211657fab9252ac7d402c8853e33d
Montandalar <jbis1337@hotmail.com>
2020-04-13 16:24:47 +1000

    Brand as ScaleImp

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -18,8 +19,9 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws IOException {
         ClassLoader classLoader = getClass().getClassLoader();
         Parent root = FXMLLoader.load(classLoader.getResource("Imperial2Metric.fxml"));
-        primaryStage.setTitle("Imperial2Metric");
+        primaryStage.setTitle("ScaleImp");
         primaryStage.setScene(new Scene(root));
+        primaryStage.getIcons().add(new Image("ScaleImp.png"));
         primaryStage.show();
     }
 }
--- build-jar.bat
+++ build-jar.bat
@@ -1,3 +1,3 @@
 javac Main.java Controller.java MeasurementModel.java SourceUnit.java
 move *.class org\imperial2metric\
-jar cfm Imperial2Metric.jar MANIFEST.MF org\* Imperial2Metric.fxml
+jar cfm ScaleImp.jar MANIFEST.MF org\* Imperial2Metric.fxml ScaleImp.png
