4bf17795670220a174ff0c39df800bac488d07fd
Montandalar <jbis1337@hotmail.com>
2020-04-13 17:27:17 +1000

    Use ant for builds

--- build-jar.bat
+++
@@ -1,3 +0,0 @@
-javac Main.java Controller.java MeasurementModel.java SourceUnit.java
-move *.class org\imperial2metric\
-jar cfm ScaleImp.jar MANIFEST.MF org\* Imperial2Metric.fxml ScaleImp.png
--- module-info.java
+++
@@ -1,9 +0,0 @@
-module org.imperial2metric {
-    exports org.imperial2metric;
-
-    requires transitive javafx.controls;
-    requires transitive javafx.graphics;
-    requires transitive javafx.fxml;
-
-    requires java.desktop;
-}
